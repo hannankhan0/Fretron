77d32482c6091768e2c8bd59d973d5c11c842aa5
Hannan Khan <hannanmostafa18@gmail.com>
2026-05-05 02:40:14 +0500

    Delete Fretron-Iteration-1 directory

--- Fretron-Iteration-1/backend/src/middleware/admin.middleware.js
+++
@@ -1,7 +0,0 @@
-import { ApiError } from '../utils/ApiError.js';
-
-export function requireAdmin(req, _res, next) {
-  if (!req.user) return next(new ApiError(401, 'Authentication required'));
-  if (req.user.role !== 'admin') return next(new ApiError(403, 'Admin access required'));
-  next();
-}
